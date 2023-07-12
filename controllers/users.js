d5e3ff7efd08fa0acde7965e282f974250df8f35
Svetlana Naumova <DrSvetaNaumova@yandex.ru>
2023-07-12 15:48:21 +0300

    sprint13-draft1.4

--- controllers/users.js
+++ controllers/users.js
@@ -54,18 +54,13 @@ module.exports.updateUserAvatar = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        res.status(404).send({ message: 'Пользователь не существует.' });
-        return;
-      }
-      res.status(200).send(user);
-    })
+    .then((user) => res.status(200).send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({
-          message: 'Переданы некорректные данные id пользователя.',
-        });
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные id пользователя.' });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(400).send({ message: 'Пользователь не существует.' });
       }
       return res.status(500).send({ message: 'Произошла ошибка.' });
     });
