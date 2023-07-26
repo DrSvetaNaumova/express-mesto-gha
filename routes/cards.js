3fd37acbe786f26fc0f4b281cd63254e6187b50e
Svetlana Naumova <DrSvetaNaumova@yandex.ru>
2023-07-26 16:08:52 +0300

    sprint14-draft0.10

--- routes/cards.js
+++ routes/cards.js
@@ -18,19 +18,54 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
-      link: Joi.string().pattern(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/),
+      link: Joi.string().pattern(
+        /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/,
+      ),
     }),
   }),
   createCard,
 );
 
 // ставит лайк
-router.put('/:cardId/likes', likeCard);
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      link: Joi.string().pattern(
+        /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/,
+      ),
+    }),
+  }),
+  likeCard,
+);
 
 // удаляет лайк
-router.delete('/:cardId/likes', dislikeCard);
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      link: Joi.string().pattern(
+        /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/,
+      ),
+    }),
+  }),
+  dislikeCard,
+);
 
 // удаляет карточку
-router.delete('/:cardId', deleteCard);
+router.delete(
+  '/:cardId',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      link: Joi.string().pattern(
+        /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/,
+      ),
+    }),
+  }),
+  deleteCard,
+);
 
 module.exports = router;
